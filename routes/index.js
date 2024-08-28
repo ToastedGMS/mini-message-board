b2908ba8a0ef53715773682b7278e0a8614fb8a7
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-08-28 10:47:50 -0300

    change code to store messages in database instead of array

--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,36 @@
 const express = require('express');
 const router = express.Router();
 const { format } = require('date-fns');
-
-//initialize message object
-const messages = [
-    {
-      text: "Click the New Message link to send a new message!",
-      user: "Admin",
-      added: format(new Date(), 'PPpp')
-    }
-];
+const { insertMessage, fetchMessages } = require('../database/queries');
   
 //handle get requests; populate template with message object
-router.get('/', (req,res) => {
-    res.render("index", { title: "Mini Messageboard", messages: messages })
-})
+router.get('/', async (req, res) => {
+    try {
+        const messages = await fetchMessages();
+        res.render('index', { messages }); 
+    } catch (err) {
+        console.error('Error fetching messages:', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 //handle post requests
-router.post('/new', (req,res) => {
-    const user = req.body.author;
-    const text = req.body.message;
+router.post('/new', async (req, res) => {
+    const message = {
+        user: req.body.author,
+        text: req.body.message,
+        added: format(new Date(), 'PPpp') // Format the date
+    };
 
-    messages.push({ text, user, added: format(new Date(), 'PPpp') });
+    try {
+        await insertMessage(message);
+        res.redirect("/"); 
+    } catch (err) {
+        console.error('Error handling POST request:', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
-    res.redirect("/")
-})
 
 module.exports = router;
